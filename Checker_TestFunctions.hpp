93cd02476f3300d34f5854b58c81b39b04c1aa6d
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-07-05 18:00:42 +0530

    Update Checker_TestFunctions.hpp

--- Checker_TestFunctions.hpp
+++ Checker_TestFunctions.hpp
@@ -10,6 +10,7 @@ template <typename temperature, class Charge_Discharge_SOC_Temp>
 bool ChargeTemp_SOC_CRate_Check(temperature temp_SOC, Charge_Discharge_SOC_Temp ClsName) {
 
   if(temp_SOC <= ClsName.Lower_Threshold || temp_SOC >= ClsName.Upper_Threshold) {
+    cout << "Charging Temperature/State Of Charge/Charge Rate out of range!\n";
     return false;
   }
   return true;
@@ -24,17 +25,8 @@ bool batteryIsOk(temperature temp, soc State_Of_Charge, chargeRate CRate) {
   ChargeRate CRate_temp_obj;
 
   bool chargetemp = ChargeTemp_SOC_CRate_Check<temperature, Charge_Temperature>(temp, Charge_temp_obj);
-  if(!chargetemp){
-    cout << "Warning: Charging Temperature out of range!\n";
-  }
   bool SOC = ChargeTemp_SOC_CRate_Check<soc, StateOfCharge>(State_Of_Charge, Soc_temp_obj);
-  if(!SOC){
-    cout << "Warning: State Of Charge out of range!\n";
-  }
   bool Charge_Rate = ChargeTemp_SOC_CRate_Check<chargeRate, ChargeRate>(CRate, CRate_temp_obj);
-  if(!Charge_Rate){
-    cout << "Warning: Charge Rate out of range!\n";
-  }
 
   if((chargetemp && SOC && Charge_Rate) == true){
     return true;
