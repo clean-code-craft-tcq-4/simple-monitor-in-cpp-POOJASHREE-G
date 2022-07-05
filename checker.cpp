c4bf2178d99d2de8bd0dd358b88760e7eca5bc84
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-07-05 17:36:16 +0530

    Update checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -3,6 +3,8 @@
 #include "Checker_TestFunctions.hpp"
 using namespace std;
 
+extern template bool batteryIsOk<float, float, float>();
+
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
