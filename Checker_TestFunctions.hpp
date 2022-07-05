268a7211b0030c1efd477bfc171f0668dfea19dc
POOJASHREE-G <105204354+POOJASHREE-G@users.noreply.github.com>
2022-07-05 17:57:37 +0530

    Update Checker.cpp

--- Checker_TestFunctions.hpp
+++ Checker_TestFunctions.hpp
@@ -1,10 +1,45 @@
 #ifndef CHECKER_TESTFUNCTIONS_HPP
 #define CHECKER_TESTFUNCTIONS_HPP
 
+#include <iostream>
+#include "Checker_TestFunctions.hpp"
+#include "Standard_Battery_Specifications.hpp"
+using namespace std;
+
 template <typename temperature, class Charge_Discharge_SOC_Temp>
-bool ChargeTemp_SOC_CRate_Check(temperature temp_SOC, Charge_Discharge_SOC_Temp ClsName);
+bool ChargeTemp_SOC_CRate_Check(temperature temp_SOC, Charge_Discharge_SOC_Temp ClsName) {
+
+  if(temp_SOC <= ClsName.Lower_Threshold || temp_SOC >= ClsName.Upper_Threshold) {
+    return false;
+  }
+  return true;
+}
+
 
 template <typename temperature, typename soc, typename chargeRate>
-bool batteryIsOk(temperature temp, soc State_Of_Charge, chargeRate CRate);
+bool batteryIsOk(temperature temp, soc State_Of_Charge, chargeRate CRate) {
+
+  Charge_Temperature Charge_temp_obj;
+  StateOfCharge Soc_temp_obj;
+  ChargeRate CRate_temp_obj;
+
+  bool chargetemp = ChargeTemp_SOC_CRate_Check<temperature, Charge_Temperature>(temp, Charge_temp_obj);
+  if(!chargetemp){
+    cout << "Warning: Charging Temperature out of range!\n";
+  }
+  bool SOC = ChargeTemp_SOC_CRate_Check<soc, StateOfCharge>(State_Of_Charge, Soc_temp_obj);
+  if(!SOC){
+    cout << "Warning: State Of Charge out of range!\n";
+  }
+  bool Charge_Rate = ChargeTemp_SOC_CRate_Check<chargeRate, ChargeRate>(CRate, CRate_temp_obj);
+  if(!Charge_Rate){
+    cout << "Warning: Charge Rate out of range!\n";
+  }
+
+  if((chargetemp && SOC && Charge_Rate) == true){
+    return true;
+  }
+  return false;
+}
 
 #endif
--- checker.cpp
+++ checker.cpp
@@ -3,8 +3,6 @@
 #include "Checker_TestFunctions.hpp"
 using namespace std;
 
-extern template bool batteryIsOk<float, float, float>();
-
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
